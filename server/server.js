e627872ee650b5b894614e0618b2d5961013ac8f
nhantran1711 <traty191@mymail.unisa.edu.au>
2025-05-11 15:05:22 +0930

    Set up API integration for external API, get data

--- server/server.js
+++ server/server.js
@@ -1,16 +1,60 @@
 const express = require("express");
-const app = express();
 const cors = require("cors");
+
+const app = express();
+const PORT = 4000;
+
 const corsOptions = {
     origin: ["http://localhost:5173"]
 }
-
 app.use(cors(corsOptions));
 
-app.get("/api", (req, res) => {
-    res.json()
+// Fetch API data from NASA EONET
+const fetchAPIEONET = async () => {
+    const res = await fetch("https://eonet.gsfc.nasa.gov/api/v3/events");
+    const eonetdata = await res.json();
+
+    if (!res.ok) {
+        console.error("Fetch data fail", res.status);
+        throw new Error("Fail to reach EONET data");
+    }
+
+    // Format
+    const eonetFormat = eonetdata.events.map((event) => {
+        const id = event.id;
+        const title = event.title;
+        const categories = event.categories?.[0]?.title || "Unknown";
+        const geometry = event.geometry?.[0] || {};
+        const magnitude = geometry.magnitudeValue || null;
+        const date = geometry.date || null;
+        const longitude = geometry.coordinates?.[0] || null;
+        const latitude = geometry.coordinates?.[1] || null;
+    
+        return {
+            id,
+            title,
+            categories,
+            magnitude,
+            date,
+            longitude,
+            latitude,
+            source: "NASA EONET" 
+        }})
+    return eonetFormat;
+}
+
+// API route to fetch data from EONET
+app.get("/api/eonet", async (req, res) => {
+    try {
+        const eonetData = await fetchAPIEONET();
+        res.json(eonetData);
+    }
+    catch (error) {
+        console.log(`Error fetching data: ${error}`);
+        res.status(500).json({message: "Unable to fetch data from EONET"})
+    }
 })
 
-app.listen(8080, () =>  {
-    console.log("Server is listening on PORT 8080")
+app.listen(4000, () =>  {
+    console.log(`Server is listening on ${PORT}`);
 })
